4bda0a6412d913791de9480d6641158c832cef0e
womoak75 <womo@koller.it>
2024-03-06 22:43:42 +0100

    enable BridgeHttp injection in DummyBridgeHttpFactory

--- io.openems.edge.bridge.http/src/io/openems/edge/bridge/http/dummy/DummyBridgeHttpFactory.java
+++ io.openems.edge.bridge.http/src/io/openems/edge/bridge/http/dummy/DummyBridgeHttpFactory.java
@@ -11,16 +11,28 @@ import io.openems.edge.bridge.http.api.BridgeHttpFactory;
 
 public class DummyBridgeHttpFactory extends BridgeHttpFactory {
 
+	private BridgeHttp bridgeHttp;
+
 	public DummyBridgeHttpFactory() throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
 		super();
-		ReflectionUtils.setAttribute(BridgeHttpFactory.class, this, "csoBridgeHttp", new DummyBridgeHttpCso());
+		ReflectionUtils.setAttribute(BridgeHttpFactory.class, this, "csoBridgeHttp", new DummyBridgeHttpCso(this));
+	}
+	
+	public void setBridgeHttp(BridgeHttp bridgeHttp) {
+		this.bridgeHttp = bridgeHttp;
 	}
 
 	private static class DummyBridgeHttpCso implements ComponentServiceObjects<BridgeHttp> {
 
+		private DummyBridgeHttpFactory factory;
+
+		public DummyBridgeHttpCso() {}
+		public DummyBridgeHttpCso(DummyBridgeHttpFactory factory) {
+			this.factory = factory;
+		}
 		@Override
 		public BridgeHttp getService() {
-			return new DummyBridgeHttp();
+			return factory != null ? factory.bridgeHttp : new DummyBridgeHttp();
 		}
 
 		@Override
