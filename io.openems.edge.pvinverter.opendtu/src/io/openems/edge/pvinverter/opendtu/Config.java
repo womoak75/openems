61e7f3041bfc8aefab2dd4d52834249f3730bd92
womoak75 <womo@koller.it>
2024-03-08 14:56:58 +0100

    fix typo in config

--- io.openems.edge.pvinverter.opendtu/src/io/openems/edge/pvinverter/opendtu/Config.java
+++ io.openems.edge.pvinverter.opendtu/src/io/openems/edge/pvinverter/opendtu/Config.java
@@ -6,8 +6,8 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 import io.openems.edge.meter.api.SinglePhase;
 
 @ObjectClassDefinition(//
-		name = "io.openems.edge.pvinerter.opendtu", //
-		description = "")
+		name = "io.openems.edge.pvinverter.opendtu", //
+		description = "OpenDTU supported PVInverters")
 @interface Config {
 
 	@AttributeDefinition(name = "Component-ID", description = "Unique ID of this Component")
@@ -19,23 +19,23 @@ import io.openems.edge.meter.api.SinglePhase;
 	@AttributeDefinition(name = "Is enabled?", description = "Is this Component enabled?")
 	boolean enabled() default true;
 
-	String webconsole_configurationFactory_nameHint() default "io.openems.edge.pvinerter.opendtu [{id}]";
-	
+	String webconsole_configurationFactory_nameHint() default "io.openems.edge.pvinverter.opendtu [{id}]";
+
 	@AttributeDefinition(name = "Inverter serial", description = "serial of inverter")
 	String inverterSerial() default "";
-	
+
 	@AttributeDefinition(name = "Hostname", description = "Ip/Hostname of openDTU")
 	String openDtuHost() default "localhost";
 
 	@AttributeDefinition(name = "Port", description = "Port of openDTU")
 	int openDtuPort() default 80;
-	
+
 	@AttributeDefinition(name = "Protocol", description = "http / https")
 	String openDtuSchema() default "http";
-	
+
 	@AttributeDefinition(name = "Username", description = "Username for authorization")
 	String openDtuUser() default "admin";
-	
+
 	@AttributeDefinition(name = "Password", description = "Username for authorization")
 	String openDtuPass() default "admin";
 
